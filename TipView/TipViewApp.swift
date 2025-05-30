58835e100a006dd4af51582416f5f69b737775e7
Hyowon-Cho <ardese@naver.com>
2025-05-30 17:02:43 +0900

    Initial Commit

---
+++ TipView/TipViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipViewApp.swift
+//  TipView
+//
+//  Created by 조효원 on 5/30/25.
+//
+
+import SwiftUI
+
+@main
+struct TipViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TipViewTests/TipViewTests.swift
@@ -0,0 +1,17 @@
+//
+//  TipViewTests.swift
+//  TipViewTests
+//
+//  Created by 조효원 on 5/30/25.
+//
+
+import Testing
+@testable import TipView
+
+struct TipViewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
